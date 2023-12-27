9f8432c32654921ce6a5cc314b4582c05aae83e9
SurabhiGupta17 <surabhi.gupta1617@gmail.com>
2023-12-27 11:30:39 +0530

    Create launch file

---
+++ src/turtlesim_game_bringup/launch/turtlesim_game.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    spawn_turtle = Node(
+        package="turtlesim_game",
+        executable="spawn_turtle"
+    )
+
+    control_turtle = Node(
+        package="turtlesim_game",
+        executable="control_turtle"
+    )
+
+    turtlesim_node = Node(
+        package="turtlesim",
+        executable="turtlesim_node"
+    )
+
+    ld.add_action(turtlesim_node)
+    ld.add_action(spawn_turtle)
+    ld.add_action(control_turtle)
+    
+    return ld
